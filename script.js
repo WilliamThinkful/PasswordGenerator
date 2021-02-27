ae6f30de70a9c00b47df53a7e42db2c85311d8b5
Will Pottratz <wbpott09@gmail.com>
2021-02-26 21:40:27 -0600

    prompt one complete

--- script.js
+++ script.js
@@ -4,11 +4,39 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+var passwordCriteria = ["","",0];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+  var response1 = prompt("Include special charaters?", "yes/no");
+
+  if (response1 !== "yes" && response1 !== "no"){
+    alert("please provide a valid response");
+  }
+  if (response1 == "yes"){
+    passwordCriteria.[0] = "yes";
+    return secondPrompt()
+  }
+  if (response1 == "no"){
+    passwordCriteria.[0] = "yes";
+    return secondPrompt()
+  }
+}
+
+function secondPrompt() {
+  var response2 = prompt("Include uppercase letters?", "yes/no");
+  if (response2 !== "yes" && response2 !== "no"){
+    alert("please provide a valid response");
+  }
+  if ()
+
+}
