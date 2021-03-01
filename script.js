ab99154181bcac5f7e4765e7cd153fd12dc090a4
Will Pottratz <wbpott09@gmail.com>
2021-03-01 00:05:48 -0600

    debugging

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCaseArr = ["qwertyuiopasdfghjklzxcvbnm"]
-var upperCaseArr = ["QWERTYUIOPASDFGHJKLZXCVBNM"]
-var numbersArr = ["1234567890"]
-var specialArr = ["!@#$%^&*()_+-=/?."]
+var lowerCaseArr = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+var upperCaseArr = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
+var numbersArr = ["1","2","3","4","5","6","7","8","9","0"]
+var specialArr = ["!","@","#","$","%","^","&","*","(",")","_","+","-","=","/","?","."]
 var passwordCriteria = [];
 var numberLength;
 var randomizedPassword = [];
@@ -12,20 +12,22 @@ var randomizedPassword = [];
 
 function writePassword() {
   generatePassword();
-  //console.log(generatePassword())
+ console.log(numberLength)
+  randomize()
+  console.log("working 1")
   var passwordText = document.querySelector("#password");
 
   function randomize() {
     for (var i = 0; i < numberLength.length; i++) {
       var special = math.floor(math.random() * (numberLength.length -1)); i++;
- randomizedPassword.push(passwordCriteria[special])}
-
-  }
+      randomizedPassword.push(passwordCriteria[special])}
+  console.log("working 2")
+  
+  console.log(numberLength)
 
   passwordText.value = randomizedPassword;
-
+  }
 }
-
 
 
 // Add event listener to generate button
@@ -40,46 +42,48 @@ function generatePassword(){
   function firstPrompt() {
     var firstResponse = confirm("Include special charaters?");
     if (firstResponse == true){
-      passwordCriteria.push(specialArr);
+      //passwordCriteria.push(specialArr);
+      passwordCriteria.concat(specialArr)
+      console.log(passwordCriteria)
     }
-  
-    //if (firstResponse == false){
-    //passwordCriteria.push("no");
-    //return secondPrompt()
-    //}
   }
+
   //function to include uppercase letters
   function secondPrompt() {
     var secondResponse = confirm("Include uppercase letters?");
     if (secondResponse == true) {
       passwordCriteria.push(upperCaseArr)
     }
-    //if (secondResponse == false) {
-      //passwordCriteria.push("no")
-    //}
-
   }
+
   //function to include numbers
   function thirdPrompt() {
     var thirdResponse = confirm("include numbers?");
     if (thirdResponse == true) {
-      passwordCriteria.push(numberArr)
+      passwordCriteria.push(numbersArr)
+      console.log("working 3")
     }
+  }
 
   //function to confirm character count
-
   function characterCount() {
     var finalResponse = prompt("Please enter character length", "Minimum of 8 characters and a maximum of 120");
     if (finalResponse >= 8 && finalResponse <= 120) {
       var numberLength = finalResponse
-      return numberLength
+      
+      console.log(numberLength)
+      //return numberLength
     }
-    console.log(numberLength)
+    
   }
   firstPrompt()
   secondPrompt()
   thirdPrompt()
   characterCount()
+  
+}
 
-}
-}
+console.log("working count")
+console.log(numberLength)
+console.log("working count")
+
